dc1ced6609ca8b7d526ef5e3e7f67371ad75b5aa
Michal Radomski <michalra@op.pl>
2021-11-15 20:56:52 +0100

    Next.js - last update

--- 03 Next.js Tutorial/01 App_01/pages/index.tsx
+++ 03 Next.js Tutorial/01 App_01/pages/index.tsx
@@ -1,10 +1,14 @@
 import type {NextPage} from "next";
 import React from "react";
+import Link from "next/link";
 
 const Home: NextPage = (): JSX.Element => {
   return (
     <React.Fragment>
       <h1>First Next.js App</h1>
+      <Link href={`/memes/`}>
+        <a>Go to memes</a>
+      </Link>
     </React.Fragment>
   );
 };
