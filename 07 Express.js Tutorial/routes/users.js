6fdd83dc69f01b68b4ff7756758ea5d59ffb9f37
Michal Radomski <michalra@op.pl>
2021-11-19 16:01:07 +0100

    JS->TS migration done, TS->JS compilation done

--- 07 Express.js Tutorial/routes/users.js
+++ 07 Express.js Tutorial/routes/users.js
@@ -1,53 +1,43 @@
-const express = require("express");
-const router = express.Router();
-
-// console.log("router:", router);
-
+var express = require("express");
+var router = express.Router();
 router.use(logger);
-
-router.get("/", (req, res) => {
+router.get("/", function (req, res) {
   console.log("req.query.name:", req.query.name);
   res.send("User List");
 });
-
-router.get("/new", (_req, res) => {
+router.get("/new", function (_req, res) {
   res.render("users/new");
 });
-
-router.post("/", (req, res) => {
-  const isValid = false;
+router.post("/", function (req, res) {
+  var isValid = false;
   if (isValid) {
     users.push({firstName: req.body.firstName});
-    res.redirect(`/users/${users.length - 1}`);
+    res.redirect("/users/".concat(users.length - 1));
   } else {
     console.log("Error");
     res.render("users/new", {firstName: req.body.firstName});
   }
 });
-
 router
   .route("/:id")
-  .get((req, res) => {
+  .get(function (req, res) {
     console.log("req.user:", req.user);
-    res.send(`Get User With ID ${req.params.id}`);
+    res.send("Get User With ID ".concat(req.params.id));
   })
-  .put((req, res) => {
-    res.send(`Update User With ID ${req.params.id}`);
+  .put(function (req, res) {
+    res.send("Update User With ID ".concat(req.params.id));
   })
-  .delete((req, res) => {
-    res.send(`Delete User With ID ${req.params.id}`);
+  ["delete"](function (req, res) {
+    res.send("Delete User With ID ".concat(req.params.id));
   });
-
-const users = [{name: "Kyle"}, {name: "Sally"}];
-router.param("id", (req, _res, next, id) => {
+var users = [{name: "Kyle"}, {name: "Sally"}];
+router.param("id", function (req, _res, next, id) {
   console.log("id:", id);
   req.user = users[id];
   next();
 });
-
 function logger(req, _res, next) {
   console.log("req.originalUrl:", req.originalUrl);
   next();
 }
-
 module.exports = router;
--- 07 Express.js Tutorial/server.ts
+++ 07 Express.js Tutorial/server.ts
@@ -1,9 +1,12 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 app.set("view engine", "ejs");
+
 //- Moved to: /routes/users.js
 // app.get("/", (_req, res) => {
 //   res.render("index", {text: "Welcome..."});
@@ -11,7 +14,10 @@ app.set("view engine", "ejs");
 // app.get("/users", (_req, res) => {
 //   res.render("index", {text: "Users"});
 // });
-var userRouter = require("./routes/users");
+
+const userRouter = require("./routes/users");
+
 app.use("/users", userRouter);
+
 console.log("server is running...");
 app.listen(3000);
